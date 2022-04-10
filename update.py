a04e798979c085518c01bd6cd070319591b998c2
kumarvincent <62093475+kumarvincent@users.noreply.github.com>
2022-04-10 14:39:29 +0530

    Update update.py

--- update.py
+++ update.py
@@ -32,16 +32,13 @@ except TypeError:
 load_dotenv('config.env', override=True)
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
-UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
+
 try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except TypeError:
-    UPSTREAM_REPO = "https://github.com/anasty17/mirror-leech-telegram-bot"
-try:
-    if len(UPSTREAM_BRANCH) == 0:
-       raise TypeError
-except TypeError:
+    UPSTREAM_REPO = "https://github.com/kumarvincent/Mirror"
+
     UPSTREAM_BRANCH = 'master'
 
 if ospath.exists('.git'):
